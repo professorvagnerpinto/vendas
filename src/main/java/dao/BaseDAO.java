27d81f1dec126f90b7fa2117e64f8aad73242ee2
Vagner Pinto <professor.vagnerpinto@gmail.com>
2021-04-17 15:02:29 -0300

    construção dos métodos selectProdutos e selectProdutoById

--- src/main/java/dao/BaseDAO.java
+++ src/main/java/dao/BaseDAO.java
@@ -10,7 +10,7 @@ public class BaseDAO {
 		try {
 			//a string com a url para o banco de dados
 			//sintaxe: protocolo:tecnologia://domínioDoServidor:porta/database
-			String url = "jdbc:mariadb://localhost:3306/vendas";
+			final String url = "jdbc:mariadb://localhost:3306/vendas";
 			//argumentos: url para o banco, usuário, senha.
 			//retorna um objeto da classe Connection (do pacote java.sql -> que segue a especificação JDBC).
 			return DriverManager.getConnection(url, "mariadb", "mariadb");
--- src/main/java/dao/ProdutoDAO.java
+++ src/main/java/dao/ProdutoDAO.java
@@ -11,41 +11,65 @@ import model.Produto;
 
 public class ProdutoDAO extends BaseDAO{
 	//SELEC * produtos;
-	public List<Produto> getProdutos(){
+	public static List<Produto> selectProdutos() {
 		final String sql = "SELECT * FROM produtos";
-		try //try-with-resource
-		( 
+		try //try-witch-resource
+			(
 				Connection conn = getConnection();
 				PreparedStatement pstmt = conn.prepareStatement(sql);
 				ResultSet rs = pstmt.executeQuery();
-				
-		){
+			)
+		{
 			List<Produto> produtos = new ArrayList<>();
 			while(rs.next()) {
 				produtos.add(resultsetToProduto(rs));
 			}
 			return produtos;
-		}catch(SQLException e) {
+		} catch(SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public static Produto selectProdutoById(Long id) {
+		final String sql = "SELECT * FROM produtos WHERE id=?";
+		try
+		(
+			Connection conn = getConnection();
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+		)
+		{
+			pstmt.setLong(1, id);	
+			ResultSet rs = pstmt.executeQuery();
+			Produto produto = null;
+			if(rs.next()) {
+				produto = resultsetToProduto(rs);
+			}
+			rs.close();
+			return produto;
+			
+		} catch(SQLException e) {
 			e.printStackTrace();
 			return null;
 		}
 	}
 	
 	//método utilitário, converte ResultSet na classe de modelo (nesse caso, Produto)
-    private Produto resultsetToProduto(ResultSet rs) throws SQLException {
-        Produto p = new Produto();
-        p.setId(rs.getLong("id"));
-        p.setNome(rs.getString("nome"));
-        p.setDescricao(rs.getString("descricao"));
-        p.setValor(rs.getDouble("valor"));
-        p.setEstoque(rs.getInt("quantidade"));
-        p.setSituacao(rs.getBoolean("situacao"));
-        return p;
-    }
+	private static Produto resultsetToProduto(ResultSet rs) throws SQLException {
+		Produto p = new Produto();
+		p.setId(rs.getLong("id"));
+		p.setNome(rs.getString("nome"));
+		p.setDescricao(rs.getString("descricao"));
+		p.setValor(rs.getDouble("valor"));
+		p.setEstoque(rs.getInt("quantidade"));
+		p.setSituacao(rs.getBoolean("situacao"));
+
+		return p;
+	}
+      
     
     public static void main(String[] args) {
-		ProdutoDAO pDAO = new ProdutoDAO();
-		System.out.println(pDAO.getProdutos());
+		System.out.println(selectProdutoById(1L));
 	}
 
 }//fim classe
